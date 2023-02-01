ad8b0538a366a1688a7747fb4ce6704170566315
Cameron <cameroncooney@outlook.com>
2023-02-01 16:08:40 +0000

    Initial Commit

---
+++ anime/animeApp.swift
@@ -0,0 +1,17 @@
+//
+//  animeApp.swift
+//  anime
+//
+//  Created by Cameron Cooney on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct animeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
